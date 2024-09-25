9a7ce310dd8dc7829690bff984934bef57b84e66
Ricardo Gomez <95481231+RickybonMx@users.noreply.github.com>
2024-09-24 22:21:27 -0600

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -6,6 +7,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(x => x
   .AllowAnyHeader()
   .AllowAnyMethod()
